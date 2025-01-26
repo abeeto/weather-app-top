d687062a498f60773968ecd60baab36e9a6e2fea
Abhinav Thota <59943893+abeeto@users.noreply.github.com>
2025-01-26 12:27:18 +0530

    Searchbar that shows status of search

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
 import "./styles.css";
-import { askForLocation, weekWeatherByLocation } from "./weatherByLocation";
+import { getLocationSearch, weekWeatherByLocation } from "./weatherByLocation";
 import LocationForecast from "./LocationForecast";
 import WeatherDay from "./WeatherDay";
 
-const location = await askForLocation();
-const weatherResp = await weekWeatherByLocation(location);
-const newForecast = new LocationForecast({
-  resolvedAddress: weatherResp.resolvedAddress,
-  currentDay: new WeatherDay(weatherResp.days[0]),
-  next7days: weatherResp.days
-    .slice(1)
-    .map((dayJSON) => new WeatherDay(dayJSON)),
+// const location = await searchForLocation();
+async function createWeatherForecast(location) {
+  const weatherResp = await weekWeatherByLocation(location);
+  const newForecast = new LocationForecast({
+    resolvedAddress: weatherResp.resolvedAddress,
+    currentDay: new WeatherDay(weatherResp.days[0]),
+    next7days: weatherResp.days
+      .slice(1)
+      .map((dayJSON) => new WeatherDay(dayJSON)),
+  });
+  return newForecast;
+}
+const locationSearchSubmit = document.querySelector("#location-form-submit");
+const searchStatusLabel = document.querySelector("#search-status");
+locationSearchSubmit.addEventListener("click", (e) => {
+  e.preventDefault();
+  const location = getLocationSearch();
+  createWeatherForecast(location)
+    .then((newForecast) => {
+      console.log(
+        `Name: ${newForecast.locationName} \n Today: ${newForecast.currentDay.toString()} \n Next Week: \n${newForecast.next7days.map((day) => day.toString()).join("\n")}`,
+      );
+      searchStatusLabel.textContent = "FOUND!";
+    })
+    .catch((err) => {
+      searchStatusLabel.textContent = err;
+    });
+  searchStatusLabel.textContent = "Searching...";
 });
+
 // eslint-disable-next-line no-console
-console.log(
-  `Name: ${newForecast.locationName} \n Today: ${newForecast.currentDay.toString()} \n Next Week: \n${newForecast.next7days.map((day) => day.toString()).join("\n")}`,
-);
--- src/weatherByLocation.js
+++ src/weatherByLocation.js
@@ -1,27 +1,25 @@
-async function askForLocation() {
-  // eslint-disable-next-line no-alert
-  const location = await window.prompt("Enter a location:", "Hyderabad");
-  return location.toLowerCase();
-}
-
 function weekWeatherByLocation(location) {
   const weather = fetch(
     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/next7days?unitGroup=metric&include=days&key=PASBFSBRA5EJCPXZ6VTEC7FNB&contentType=json`,
     { mode: "cors" },
-  )
-    .then((resp) => {
-      if (resp.ok) {
-        return resp.json();
-      }
-      if (resp.status === 400) {
-        throw new Error("400: Bad User Request. No such location found");
-      } else {
-        throw new Error(resp.status);
-      }
-    })
-    // eslint-disable-next-line no-console
-    .catch((err) => console.error(err));
+  ).then((resp) => {
+    if (resp.ok) {
+      return resp.json();
+    }
+    if (resp.status === 400) {
+      throw new Error("400: Bad User Request. No such location found");
+    } else {
+      throw new Error(resp.status);
+    }
+  });
   return weather;
 }
 
-export { askForLocation, weekWeatherByLocation };
+function getLocationSearch() {
+  // eslint-disable-next-line no-alert
+  const searchInput = document.querySelector("#location-search-field");
+  const location = searchInput.value.toLowerCase();
+  return location;
+}
+
+export { getLocationSearch, weekWeatherByLocation };
